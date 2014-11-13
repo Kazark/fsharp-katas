c2d7dad9c86b8d178edb4f0cb192c022fa479c27
Kazark <kazark@zoho.com>
2014-11-13 10:24:31 -0500

    Bootstrapped a project for our favorite problem :)
    @dnak wrote it, but before I look at his, I want to write it myself,
    because I have never attempted a purely functional solution to it
    before, and have been greatly looking forward to doing it. Also, the
    processes I am trying to run today are taking a LONG TIME, so I need
    something productive to do in the meantime. @dnak's solution is here:
    https://gist.github.com/dnak/81dd18ac7ee5b16983fc

---
+++ ConwayGameOfLife/ConwayGameOfLife.Specs/Specs.fs
@@ -0,0 +1,11 @@
+﻿namespace ConwayGameOfLife.Specs
+
+open NUnit.Framework
+open FsUnit
+open GameOfLife
+
+[<TestFixture>]
+type ``Given a living cell`` () = 
+    [<Test>]
+    member x.``which has only one living neighbors, it should die in the next generation`` () =
+        nextStateOf LivingCell 1 |> should equal DeadCell
---
+++ ConwayGameOfLife/ConwayGameOfLife/GameOfLife.fs
@@ -0,0 +1,11 @@
+﻿module GameOfLife
+
+type Cell =
+    | DeadCell
+    | LivingCell
+    override this.ToString() =
+        match this with
+        | DeadCell -> "Dead"
+        | LivingCell -> "Alive"
+
+let nextStateOf _ _ = DeadCell
